2755b6e1ce20071dd97f375d3f4cec12d71a73c7
Taufique Khan <36895499+taurtk@users.noreply.github.com>
2019-07-05 02:01:40 +0530

    Create Realtimefacedetection.py

---
+++ Realtimefacedetection.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 02:33:45 2019
+
+@author: Taufique
+"""
+import numpy as np
+import cv2
+face_cascade=cv2.CascadeClassifier('C:/Users/monu/Desktop/haarcascade_frontalface_default.xml')
+videocapture = cv2.VideoCapture(0)
+scale_factor=1.3
+while 1:
+    ret, pic = videocapture.read()
+    faces = face_cascade.detectMultiScale(pic, scale_factor, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(pic,(x,y),(x+w,y+h),(255,0,0),2)
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(pic,'Taufique',(x,y),font,2,(255,255,255),2,cv2.LINE_AA)
+    print("Number of faces found{}".format(len(faces)))
+    cv2.imshow('face',pic)
+    k=cv2.waitKey(30) & 0xff
+    if k==2:
+        break
+
+cv2.destroyAllWindows()
