a04d3663b77f8cb490e327d108f4c12d8644df64
Taufique Khan <36895499+taurtk@users.noreply.github.com>
2019-07-04 22:06:46 +0530

    Update imagethresholding.py

--- imagethresholding.py
+++ imagethresholding.py
@@ -8,7 +8,7 @@ import numpy as np
 import cv2
 pic=cv2.imread('C:/Users/monu/Pictures/Sketch.png',0)
 threshold_value=100
-(T_value, binary_threshold)=cv2.threshold(pic, threshold_value, 255, cv2.THRESH_BINARY)
+(T_value, binary_threshold)=cv2.threshold(pic, threshold_value, 255, cv2.THRESH_BINARY_INV)
 
 cv2.imshow('rotate',binary_threshold)
 cv2.waitKey(0)
